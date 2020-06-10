016d8710e4b265ae4cc5ccf8fd323861f1567a6c
Jon Bash <jonbash@protonmail.com>
2020-06-10 16:09:50 -0700

    first pass eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,11 +1,28 @@
+
 '''
 Input: an integer
 Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+'''
+cookie_cache = dict()
+
+def eating_cookies(n):
+    if n < 0:
+        return 0
+    if n in [0, 1]:
+        return 1
+    try:
+        return cookie_cache[str(n)]
+    except KeyError:
+        output = 0
+        for i in range(3):
+            x = n - i - 1
+            x_result = eating_cookies(x)
+            cookie_cache[str(x)] = x_result
+            output += x_result
+
+        return output
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
